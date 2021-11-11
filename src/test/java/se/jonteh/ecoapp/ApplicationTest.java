d874ae686a7cbef206b724dbddb162759a11ee63
Jonteh <jonathan.helsing@gmail.com>
2021-11-11 13:19:26 +0100

    Added Jacoco support to SonarCloud/Cube

---
+++ src/test/java/se/jonteh/ecoapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package se.jonteh.ecoapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ApplicationTest {
+
+    @Test
+    void testSomething() {
+        assertEquals(8, 4*2);
+    }
+}
