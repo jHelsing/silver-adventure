25bd7633f87ef1c61726689077969d6bc7b2a31e
Jonteh <jonathan.helsing@gmail.com>
2021-11-11 13:38:54 +0100

    Added Cucumber support

---
+++ src/test/java/se/jonteh/ecoapp/StepDefinitions.java
@@ -0,0 +1,11 @@
+package se.jonteh.ecoapp;
+
+import io.cucumber.java.en.Given;
+
+public class StepDefinitions {
+
+    @Given("gg")
+    public void gg() {
+        System.out.println("gg");
+    }
+}
