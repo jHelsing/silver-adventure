fbe7ef2f6585a9635e79da2f185df12795e84776
helsing <jonathan.helsing@gmail.com>
2021-11-15 11:13:06 +0100

    Resolve existing Code Smells

--- src/test/java/se/jonteh/ecoapp/ApplicationTest.java
+++ src/test/java/se/jonteh/ecoapp/ApplicationTest.java
@@ -1,10 +1,14 @@
 package se.jonteh.ecoapp;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ApplicationTest {
+import org.junit.jupiter.api.Test;
+
+/**
+ * Simple test just to ensure that the CI pipeline and various static analytics are working.
+ * Remove as soon as real tests exist!
+ */
+class ApplicationTest {
 
   @Test
   void testSomething() {
--- src/test/java/se/jonteh/ecoapp/StepDefinitions.java
+++ src/test/java/se/jonteh/ecoapp/StepDefinitions.java
@@ -2,6 +2,10 @@ package se.jonteh.ecoapp;
 
 import io.cucumber.java.en.Given;
 
+/**
+ * A Step Definitions class for Cucumber. Should maybe be extended with different classes
+ * depending on the Features.
+ */
 public class StepDefinitions {
 
   @Given("gg")
